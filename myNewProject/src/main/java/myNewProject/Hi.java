d7312e2ce139476386e7687ad93a186567062edb
Dell <Dell@DESKTOP-72RMOME>
2023-08-06 18:55:47 +0530

    see the changes

--- myNewProject/src/main/java/myNewProject/Hi.java
+++ myNewProject/src/main/java/myNewProject/Hi.java
@@ -1,11 +1,15 @@
 package myNewProject;
 
+import org.testng.Reporter;
+
 public class Hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi everyone");
 		System.out.println("Hi Amit and Syed bro ");
+		Reporter.log("Hi Everyone");
+		
 	}
 
 }
